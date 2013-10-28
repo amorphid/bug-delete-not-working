3f1d12d4d003f0ace10ce49c59e08e5c39b18b7e
Michael Pope <amorphid@captaincoder.io>
2013-10-28 12:35:36 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BugDeleteNotWorking::Application.config.secret_key_base = '935083b2bca2d840c62d46a6f740ada462453cf21c9f615e0df03950f0dea4dcf6e485d21cc5dd6b933f3572515d6253ccf825eabb5a8c20fb8a68c882f10edf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BugDeleteNotWorking::Application.config.session_store :cookie_store, key: '_bug-delete-not-working_session'
